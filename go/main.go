7851ca13969d0c962393ff65ff817dfe8af813e1
Liao Hanwen <34835605+LiaoHanwen@users.noreply.github.com>
2019-03-25 00:25:15 +0800

    Update main.go

--- go/main.go
+++ go/main.go
@@ -10,21 +10,32 @@ import (
     "log"
 )
 
+// listen port
+const PORT string = "8081"
+
 /********************************************************************
     func:   defaultRoute
     brief:  return 404 and message
     args:   w - request ResponseWriter
             r - request
-    return: 
 ********************************************************************/
 func defaultRoute(w http.ResponseWriter, r *http.Request) {
     http.Error(w, "go server command not found", 404)
 }
 
+/********************************************************************
+    func:   main
+    brief:  redirect request
+********************************************************************/
 func main() {
-    http.HandleFunc("/", defaultRoute) //设置访问的路由
+    // set default route (return 404)
+    http.HandleFunc("/", defaultRoute)
+
+    // set route for notice-search
     http.HandleFunc("/notice-search/", noticeSearch)
-    err := http.ListenAndServe(":8081", nil) //设置监听的端口
+
+    // listen port 8081
+    err := http.ListenAndServe(":" + PORT, nil)
     if err != nil {
         log.Fatal("ListenAndServe: ", err)
     }
