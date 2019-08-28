e66d2f036816c06ab9c8e1a537dea96c53736e9c
Liao Hanwen <34835605+LiaoHanwen@users.noreply.github.com>
2019-08-27 20:13:54 -0400

    mid commit

--- go/main.go
+++ go/main.go
@@ -34,7 +34,7 @@ func main() {
 	http.HandleFunc("/go/get-article-list", getArticleList)
 
 	// set listen port
-	err := http.ListenAndServe(":8081", nil)
+	err := http.ListenAndServe(":8082", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
