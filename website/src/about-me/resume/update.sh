015da53a9d45f80d5f8cfc1d2726e6fc1606e7ce
LiaoHanwen <login@liaohanwen.com>
2020-10-31 23:47:41 +0800

    Update resume

---
+++ website/src/about-me/resume/update.sh
@@ -0,0 +1,4 @@
+wget http://github.com/Poncirus/Resume/raw/master/resume.jpg -O resume.jpg &
+wget http://github.com/Poncirus/Resume/raw/master/resume.pdf -O resume.pdf &
+wget http://github.com/Poncirus/Resume/raw/master/resume-cn.jpg -O resume-cn.jpg &
+wget http://github.com/Poncirus/Resume/raw/master/resume-cn.pdf -O resume-cn.pdf &
