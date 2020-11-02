957196d44c1a97c0ba3bd050bbfb252427a73d66
LiaoHanwen <login@liaohanwen.com>
2020-11-02 23:42:00 +0800

    write about-me with react-bootstrap

--- website/src/about-me/about-me.js
+++ website/src/about-me/about-me.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom'
 
 import Accordion from 'react-bootstrap/Accordion';
 import Button from 'react-bootstrap/Button';
+import Jumbotron from 'react-bootstrap/Jumbotron'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 
@@ -27,87 +31,87 @@ class Main extends React.Component {
     }
 
     render() {
-        return <div className='container'>
-            <TopNavbar currentPage="About me"></TopNavbar>
-            <main className="container mt-4" role="main">
+        return <Container>
+            <TopNavbar currentPage='About me'></TopNavbar>
+            <Container>
                 <Resume></Resume>
                 <SocialMedia></SocialMedia>
-            </main>
-        </div>
+            </Container>
+        </Container>
     }
 }
 
 class Resume extends React.Component {
     render() {
-        return <div className="jumbotron pt-4 pb-5 shadow border">
-            <h1 className="display-4">Resume</h1>
-            <hr className="my-3" />
+        return <Jumbotron className='pt-4 pb-5 shadow border'>
+            <h1 className='display-4'>Resume</h1>
+            <hr className='my-3' />
             <Accordion>
                 <div className='mb-3'>
-                    <Accordion.Toggle as={Button} eventKey='cn' className="btn btn-secondary btn-lg mr-3">Resume - CN</Accordion.Toggle>
-                    <Accordion.Toggle as={Button} eventKey='en' className="btn btn-secondary btn-lg mr-3">Resume - EN</Accordion.Toggle>
-                    <a role='button' className="btn btn-secondary btn-lg mr-3" href='https://github.com/Poncirus/Resume' target='_blank'>View in GitHub</a>
+                    <Accordion.Toggle as={Button} eventKey='cn' className='btn btn-secondary btn-lg mr-3 mt-3'>Resume - CN</Accordion.Toggle>
+                    <Accordion.Toggle as={Button} eventKey='en' className='btn btn-secondary btn-lg mr-3 mt-3'>Resume - EN</Accordion.Toggle>
+                    <Button variant='secondary' className='btn-lg mr-3 mt-3' href='https://github.com/Poncirus/Resume' target='_blank'>View in GitHub</Button>
                 </div>
                 <Accordion.Collapse eventKey='cn'>
-                    <div className='container'>
-                        <div className='row'>
-                            <div className='col-12 col-md-10' align='center'>
+                    <Container>
+                        <Row>
+                            <Col xs={12} md={10} align='center'>
                                 <img src={ResumeCNJPG} style={{ width: '100%' }} className='border rounded shadow' />
-                            </div>
-                            <div className='col-12 col-md-2 mt-3'>
-                                <a role='button' className="btn btn-secondary mt-3" href={ResumeCNPDF} target='_blank' download>Download PDF</a>
-                                <a role='button' className="btn btn-secondary mt-3" href={ResumeCNJPG} target='_blank' download>Download JPG</a>
-                            </div>
-                        </div>
-                    </div>
+                            </Col>
+                            <Col xs={12} md={2} className='mt-3'>
+                                <Button variant='secondary' className='mt-3' href={ResumeCNPDF} target='_blank' download>Download PDF</Button>
+                                <Button variant='secondary' className='mt-3' href={ResumeCNJPG} target='_blank' download>Download JPG</Button>
+                            </Col>
+                        </Row>
+                    </Container>
                 </Accordion.Collapse>
                 <Accordion.Collapse eventKey='en'>
-                    <div className='container'>
-                        <div className='row'>
-                            <div className='col-12 col-md-10' align='center'>
+                    <Container>
+                        <Row>
+                            <Col xs={12} md={10} align='center'>
                                 <img src={ResumeJPG} style={{ width: '100%' }} className='border rounded shadow' />
-                            </div>
-                            <div className='col-12 col-md-2 mt-3'>
-                                <a role='button' className="btn btn-secondary mt-3" href={ResumePDF} target='_blank' download>Download PDF</a>
-                                <a role='button' className="btn btn-secondary mt-3" href={ResumeJPG} target='_blank' download>Download JPG</a>
-                            </div>
-                        </div>
-                    </div>
+                            </Col>
+                            <Col xs={12} md={2} className='mt-3'>
+                                <Button variant='secondary' className='mt-3' href={ResumePDF} target='_blank' download block>Download PDF</Button>
+                                <Button variant='secondary' className='mt-3' href={ResumeJPG} target='_blank' download block>Download JPG</Button>
+                            </Col>
+                        </Row>
+                    </Container>
                 </Accordion.Collapse>
             </Accordion>
-        </div>
+        </Jumbotron>
     }
 }
 
 class SocialMedia extends React.Component {
     render() {
-        return <div className="jumbotron pt-4 pb-5 shadow border">
-            <h1 className="display-4">Social Media</h1>
-            <hr className="my-3" />
-            <div className='container'>
-                <div className='row'>
-                    <div className='col-12 col-md-3' align='center'>
+        return <Jumbotron className='pt-4 pb-5 shadow border'>
+            <h1 className='display-4'>Social Media</h1>
+            <hr className='my-3' />
+            <Container>
+                <Row>
+                    <Col xs={12} md={3} align='center' className='mt-3'>
                         <a href='https://github.com/Poncirus' target='_blank' >
                             <img src={GitHubMark} style={{ height: '40px' }} />
                             <img src={GitHubLogo} style={{ height: '60px' }} />
                         </a>
-                    </div>
-                    <div className='col-12 col-md-3' align='center'>
+                    </Col>
+                    <Col xs={12} md={3} align='center' className='mt-3'>
                         <a href='https://www.zhihu.com/people/liao-han-wen-31' target='_blank' >
                             <img src={ZhihuLogo} style={{ height: '60px', padding: '10px' }} />
                         </a>
-                    </div>
-                    <div className='col-12 col-md-3' align='center'>
+                    </Col>
+                    <Col xs={12} md={3} align='center' className='mt-3'>
                         <a href='https://www.weibo.com/3502084483/profile' target='_blank' >
                             <img src={WeiboLogo} style={{ height: '60px', padding: '10px' }} />
                         </a>
-                    </div>
-                </div>
-            </div>
-        </div>
+                    </Col>
+                </Row>
+            </Container>
+        </Jumbotron>
     }
 }
 
 
 // main
-ReactDOM.render(<Main></Main >, document.getElementById('main'))
+ReactDOM.render(<Main></Main>, document.getElementById('main'))
