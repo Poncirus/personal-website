f1663cf1d64c47b0a19b0b780b78444e8f2ad3c7
Poncirus <login@liaohanwen.com>
2020-11-09 00:28:33 +0800

    add panels in home

--- website/src/home/home.js
+++ website/src/home/home.js
@@ -21,6 +21,18 @@ const panels = [{
     header: "Articles",
     intro: "My notes",
     items: [{ item: "Article List", href: "/articles/article-list" }]
+}, {
+    header: "About me",
+    intro: "My informations",
+    items: [{ item: "Informations", href: "/about-me" }]
+}, {
+    header: "VSCode",
+    intro: "Visual Studio Code online editor, powered by code-server",
+    items: [{ item: "Launch VSCode Online", href: "https://code.liaohanwen.com" }, { item: "code-server on Github", href: "https://github.com/cdr/code-server" }]
+}, {
+    header: "Cloud",
+    intro: "My cloud disk, powered by Nextcloud",
+    items: [{ item: "Cloud Disk", href: "https://cloud.liaohanwen.com" }, { item: "Nextcloud Website", href: "https://nextcloud.com/" }]
 }]
 
 ReactDOM.render(panels.map(v => <Panel key={v.header} {...v}></Panel>), document.getElementById('panels'))
