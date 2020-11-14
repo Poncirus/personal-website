36fe3b9679631d4935a9b3447ad3c092eccd6e45
Poncirus <login@liaohanwen.com>
2020-11-14 22:37:31 +0800

    rewrite main with TopNavbar

--- website/src/main/index.js
+++ website/src/main/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import TopNavbar from '@/navbar/navbar.js'
+
 import 'bootstrap/dist/css/bootstrap.min.css'
 import "@/main/cover.css";
 
@@ -9,38 +11,28 @@ import heartImg from "@/main/heart.png"
 // set title
 document.title = '橘生淮北'
 
-// heart img
-const heart = <img className="my-4" src={heartImg} alt="heart png" height="200" />
-ReactDOM.render(heart, document.getElementById('heart'))
+ReactDOM.render(<TopNavbar></TopNavbar>, document.getElementById('header'))
 
-// navbar
-function TopNavbar() {
-    const items = [{
-        str: "Home",
-        src: "/home"
-    },
-    {
-        str: "Tools",
-        src: "/tools"
-    },
-    {
-        str: "Articles",
-        src: "/articles/article-list"
-    },
-    {
-        str: "About me",
-        src: "/about-me"
-    },
-    {
-        str: "VSCode",
-        src: "https://code.liaohanwen.com"
-    },
-    {
-        str: "Cloud",
-        src: "https://cloud.liaohanwen.com"
-    }]
-
-    return items.map(v => <a className="nav-link" href={v.src} key={v.str} > {v.str} </a>)
+class Main extends React.Component {
+    render() {
+        return <div>
+            <h1 className="cover-heading">To Ling</h1>
+            <p className="lead">I will Love You and Respect You Till the End of My Life.</p>
+            <img className="my-4" src={heartImg} alt="heart png" height="200" />
+            <p className="lead">2019/3/26</p>
+        </div>
+    }
 }
 
-ReactDOM.render(<TopNavbar></TopNavbar>, document.getElementById('navbar'))
+ReactDOM.render(<Main></Main>, document.getElementById('main'))
+
+class Footer extends React.Component {
+    render() {
+        return <div>
+            <p>Website for My Love</p>
+            <a href="http://www.beian.miit.gov.cn" className="text-dark">京ICP备17044828号-1</a>
+        </div>
+    }
+}
+
+ReactDOM.render(<Footer></Footer>, document.getElementById('footer'))
