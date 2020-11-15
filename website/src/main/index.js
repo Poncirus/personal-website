ffad643b55bb62cbaa47b2362c9f7da0c8b833aa
Poncirus <login@liaohanwen.com>
2020-11-15 16:24:04 +0800

    redesign main page

--- website/src/main/index.js
+++ website/src/main/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom'
 
 import TopNavbar from '@/navbar/navbar.js'
 
+import Accordion from 'react-bootstrap/Accordion'
+import Button from 'react-bootstrap/Button'
+import Carousel from 'react-bootstrap/Carousel'
+
 import 'bootstrap/dist/css/bootstrap.min.css'
 import "@/main/cover.css";
 
@@ -15,11 +19,25 @@ ReactDOM.render(<TopNavbar></TopNavbar>, document.getElementById('header'))
 
 class Main extends React.Component {
     render() {
-        return <div>
-            <h1 className="cover-heading">To Ling</h1>
-            <p className="lead">I will Love You and Respect You Till the End of My Life.</p>
-            <img className="my-4" src={heartImg} alt="heart png" height="200" />
-            <p className="lead">2019/3/26</p>
+        return <div className="mb-4">
+            <Carousel indicators={false} fade={true} interval={4000} nextIcon={null} prevIcon={null}>
+                <Carousel.Item>
+                    <h1>My Website</h1>
+                    <h4 className="font-weight-light">Articles and Useful Tools</h4>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <h1>Articles</h1>
+                    <h4 className="font-weight-light">My Notes and thoughts</h4>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <h1>VSCode Server</h1>
+                    <h4 className="font-weight-light">Online VSCode Editor</h4>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <h1>Nextcloud</h1>
+                    <h4 className="font-weight-light">My Cloud Storage</h4>
+                </Carousel.Item>
+            </Carousel>
         </div>
     }
 }
@@ -29,7 +47,17 @@ ReactDOM.render(<Main></Main>, document.getElementById('main'))
 class Footer extends React.Component {
     render() {
         return <div>
-            <p>Website for My Love</p>
+            <Accordion>
+                <Accordion.Collapse eventKey="love">
+                    <div>
+                        <h2 className="cover-heading">To Ling</h2>
+                        <p className="lead font-weight-light">I will Love You and Respect You Till the End of My Life.</p>
+                        <img className="my-4" src={heartImg} alt="heart png" height="150" />
+                        <p className="lead font-weight-light">2019/3/26</p>
+                    </div>
+                </Accordion.Collapse>
+                <Accordion.Toggle as={Button} variant="link" eventKey="love" className="text-secondary text-decoration-none">Website for My Love</Accordion.Toggle>
+            </Accordion>
             <a href="http://www.beian.miit.gov.cn" className="text-dark">京ICP备17044828号-1</a>
         </div>
     }
