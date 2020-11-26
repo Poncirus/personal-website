4344be6e8cf52edca4a4b327cf41110973d1713f
Poncirus <login@liaohanwen.com>
2020-11-26 22:58:32 +0800

    translate and modify about-me

--- website/src/about-me/about-me.js
+++ website/src/about-me/about-me.js
@@ -22,8 +22,13 @@ import GitHubMark from '@/about-me/logo/GitHub_Mark.png'
 import ZhihuLogo from '@/about-me/logo/Zhihu_Logo.png'
 import WeiboLogo from '@/about-me/logo/Weibo_Logo.png'
 
+import { getLanguageCookie } from '@/js/lang.js'
+
+const labels = require('@/about-me/labels.json')
+const lang = getLanguageCookie()
+
 // set title
-document.title = '橘生淮北 - About me'
+document.title = '橘生淮北 - ' + labels['About me'][lang]
 
 class Main extends React.Component {
     constructor(props) {
@@ -43,14 +48,14 @@ class Main extends React.Component {
 
 class Resume extends React.Component {
     render() {
-        return <Jumbotron className='pt-4 pb-5 shadow border'>
-            <h1 className='display-4'>Resume</h1>
+        return <Jumbotron className='py-3 shadow border'>
+            <h1 className='font-weight-light'>{labels['Resume'][lang]}</h1>
             <hr className='my-3' />
             <Accordion>
-                <div className='mb-3'>
-                    <Accordion.Toggle as={Button} eventKey='cn' className='btn btn-secondary btn-lg mr-3 mt-3'>Resume - CN</Accordion.Toggle>
-                    <Accordion.Toggle as={Button} eventKey='en' className='btn btn-secondary btn-lg mr-3 mt-3'>Resume - EN</Accordion.Toggle>
-                    <Button variant='secondary' className='btn-lg mr-3 mt-3' href='https://github.com/Poncirus/Resume' target='_blank'>View in GitHub</Button>
+                <div className='my-2'>
+                    <Accordion.Toggle as={Button} eventKey='cn' className='btn btn-secondary mr-3'>{labels['Resume - CN'][lang]}</Accordion.Toggle>
+                    <Accordion.Toggle as={Button} eventKey='en' className='btn btn-secondary mr-3'>{labels['Resume - EN'][lang]}</Accordion.Toggle>
+                    <Button variant='secondary' className='mr-3' href='https://github.com/Poncirus/Resume' target='_blank'>{labels['View in GitHub'][lang]}</Button>
                 </div>
                 <Accordion.Collapse eventKey='cn'>
                     <Container>
@@ -59,8 +64,8 @@ class Resume extends React.Component {
                                 <img src={ResumeCNJPG} style={{ width: '100%' }} className='border rounded shadow' />
                             </Col>
                             <Col xs={12} md={2} className='mt-3'>
-                                <Button variant='secondary' className='mt-3' href={ResumeCNPDF} target='_blank' download>Download PDF</Button>
-                                <Button variant='secondary' className='mt-3' href={ResumeCNJPG} target='_blank' download>Download JPG</Button>
+                                <Button variant='secondary' className='mt-3' href={ResumeCNPDF} target='_blank' download>{labels['Download PDF'][lang]}</Button>
+                                <Button variant='secondary' className='mt-3' href={ResumeCNJPG} target='_blank' download>{labels['Download JPG'][lang]}</Button>
                             </Col>
                         </Row>
                     </Container>
@@ -72,8 +77,8 @@ class Resume extends React.Component {
                                 <img src={ResumeJPG} style={{ width: '100%' }} className='border rounded shadow' />
                             </Col>
                             <Col xs={12} md={2} className='mt-3'>
-                                <Button variant='secondary' className='mt-3' href={ResumePDF} target='_blank' download block>Download PDF</Button>
-                                <Button variant='secondary' className='mt-3' href={ResumeJPG} target='_blank' download block>Download JPG</Button>
+                                <Button variant='secondary' className='mt-3' href={ResumePDF} target='_blank' download block>{labels['Download PDF'][lang]}</Button>
+                                <Button variant='secondary' className='mt-3' href={ResumeJPG} target='_blank' download block>{labels['Download JPG'][lang]}</Button>
                             </Col>
                         </Row>
                     </Container>
@@ -85,23 +90,23 @@ class Resume extends React.Component {
 
 class SocialMedia extends React.Component {
     render() {
-        return <Jumbotron className='pt-4 pb-5 shadow border'>
-            <h1 className='display-4'>Social Media</h1>
+        return <Jumbotron className='py-3 shadow border'>
+            <h1 className='font-weight-light'>{labels['Social Media'][lang]}</h1>
             <hr className='my-3' />
             <Container>
                 <Row>
-                    <Col xs={12} md={3} align='center' className='mt-3'>
+                    <Col xs={12} md={3} align='center' className='mt-2'>
                         <a href='https://github.com/Poncirus' target='_blank' >
                             <img src={GitHubMark} style={{ height: '40px' }} />
                             <img src={GitHubLogo} style={{ height: '60px' }} />
                         </a>
                     </Col>
-                    <Col xs={12} md={3} align='center' className='mt-3'>
+                    <Col xs={12} md={3} align='center' className='mt-2'>
                         <a href='https://www.zhihu.com/people/liao-han-wen-31' target='_blank' >
                             <img src={ZhihuLogo} style={{ height: '60px', padding: '10px' }} />
                         </a>
                     </Col>
-                    <Col xs={12} md={3} align='center' className='mt-3'>
+                    <Col xs={12} md={3} align='center' className='mt-2'>
                         <a href='https://www.weibo.com/3502084483/profile' target='_blank' >
                             <img src={WeiboLogo} style={{ height: '60px', padding: '10px' }} />
                         </a>
