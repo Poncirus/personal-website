9cd44949239108016fac7efe37f4a4cb798375a7
Liao Hanwen <login@liaohanwen.com>
2021-04-17 17:17:10 +0800

    update resume

--- website/src/about-me/resume/update.sh
+++ website/src/about-me/resume/update.sh
@@ -1,4 +1,6 @@
-wget http://github.com/Poncirus/Resume/raw/master/resume.jpg -O resume.jpg &
-wget http://github.com/Poncirus/Resume/raw/master/resume.pdf -O resume.pdf &
-wget http://github.com/Poncirus/Resume/raw/master/resume-cn.jpg -O resume-cn.jpg &
-wget http://github.com/Poncirus/Resume/raw/master/resume-cn.pdf -O resume-cn.pdf &
+work_path=$(dirname $0)
+rm ${work_path}/*.jpg ${work_path}/*.pdf
+wget http://github.com/Poncirus/Resume/raw/master/resume.jpg -O ${work_path}/resume.jpg &
+wget http://github.com/Poncirus/Resume/raw/master/resume.pdf -O ${work_path}/resume.pdf &
+wget http://github.com/Poncirus/Resume/raw/master/resume-cn.jpg -O ${work_path}/resume-cn.jpg &
+wget http://github.com/Poncirus/Resume/raw/master/resume-cn.pdf -O ${work_path}/resume-cn.pdf &
