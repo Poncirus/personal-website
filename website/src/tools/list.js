1ca4f9df28da62c54350c97e865eef64e0629474
Poncirus <login@liaohanwen.com>
2021-04-24 23:54:36 +0800

    rewrite list with react-bootstrap

--- website/src/tools/list.js
+++ website/src/tools/list.js
@@ -1,14 +1,21 @@
 import React from 'react'
 
+import Container from 'react-bootstrap/Container'
+import Card from 'react-bootstrap/Card'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
 export default class List extends React.Component {
     constructor(props) {
         super(props)
     }
 
     render() {
-        return <main className="container mt-4" role="main">
-            {Object.entries(this.props.list).map(([k, v]) => <ListItem key={k} page={k} {...v} switch={this.props.switch}></ListItem>)}
-        </main>
+        return <Container className="mt-4">
+            <Row>
+                {Object.entries(this.props.list).map(([k, v]) => <ListItem key={k} page={k} {...v} switch={this.props.switch}></ListItem>)}
+            </Row>
+        </Container>
     }
 }
 
@@ -19,12 +26,14 @@ class ListItem extends React.Component {
     }
 
     render() {
-        return <div className="card border shadow rounded col-md-4 col-12 mb-3">
-            <div className="card-body">
-                <h4 className="card-title">{this.props.item}</h4>
-                <span className="card-text">{this.props.intro}</span>
-                <a onClick={() => { this.props.switch(this.props.page) }} className="stretched-link"></a>
-            </div>
-        </div>
+        return <Col md={4} xs={12} className="mb-3">
+            <Card className="border shadow rounded">
+                <Card.Body>
+                    <h4 className="card-title">{this.props.label}</h4>
+                    <span className="card-text">{this.props.intro}</span>
+                    <a onClick={() => { this.props.switch(this.props.page) }} className="stretched-link"></a>
+                </Card.Body>
+            </Card>
+        </Col>
     }
 }
