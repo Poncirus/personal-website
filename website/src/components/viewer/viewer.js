96e9d224a12e528704b74c7940f77721e9119296
Poncirus <login@liaohanwen.com>
2021-05-22 23:54:38 +0800

    fix article display

--- website/src/components/viewer/viewer.js
+++ website/src/components/viewer/viewer.js
@@ -4,16 +4,9 @@ import ReactMarkdown from 'react-markdown'
 import 'github-markdown-css/github-markdown.css'
 
 export default class Editor extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            text: 'New input for **EasyMDE**'
-        }
-    }
-
     render() {
         return <div className='markdown-body py-3 px-3 rounded shadow border mb-3'>
-            <ReactMarkdown source={this.props.Markdown} />
+            <ReactMarkdown>{this.props.Markdown}</ReactMarkdown>
         </div>
     }
 }
